d2c7290b81ee1e407b63f39cff7def9a7ba1c670
matthaywardwebdesign <matt@matthayward.com.au>
2017-09-13 12:22:49 +1000

    Added react-keycloak

--- client/views/app/App.jsx
+++ client/views/app/App.jsx
@@ -1,11 +1,14 @@
 import './app.scss';
 import React, { Component } from 'react';
 import Routes from './Routes.jsx';
-import { func } from 'prop-types';
+import { func, object } from 'prop-types';
+import { Keycloak, KeycloakLogin } from '@genny-project/keycloak-react';
+import keycloakAdapter from 'keycloak-js';
 
 class App extends Component {
   static propTypes = {
     appStart: func,
+    keycloak: object,
   };
 
   componentDidMount() {
@@ -17,14 +20,26 @@ class App extends Component {
   }
 
   render() {
+    const { config } = this.props.keycloak;
+
+    /* Render nothing if we haven't yet received the keycloak config */
+    if ( !config ) {
+      return (
+        <div className='app' />
+      );
+    }
+
     return (
-      <div className='app'>
-        <main>
-          <content>
-            <Routes />
-          </content>
-        </main>
-      </div>
+      <Keycloak config={config} adapter={keycloakAdapter} defaultRedirectUri={'http://localhost:4000/'}>
+        <div className='app'>
+          <main>
+            <content>
+              <Routes />
+            </content>
+          </main>
+        </div>
+        <KeycloakLogin />
+      </Keycloak>
     );
   }
 }
