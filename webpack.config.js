88b315c90694050aa9cb6109d316ab24dd772a3e
adamcrow64 <adamcrow63@gmail.com>
2017-09-14 18:00:02 +1000

    Fixed bugs

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devServer: {
     contentBase: path.resolve( __dirname, './build' ),
     inline: true,
-    port: 4000,
+    port: 3000,
     historyApiFallback: true,
   },
   devtool: 'eval',
