37f598f03bc1d7e7a53673d4f2152f04fc540a87
matthaywardwebdesign <matt@matthayward.com.au>
2017-09-16 16:59:49 +1000

    Added bucketview

--- client/views/components/bucket-view/BucketView.jsx
+++ client/views/components/bucket-view/BucketView.jsx
@@ -1,10 +1,25 @@
+import './bucketView.scss';
 import React, { Component } from 'react';
+import { array, object } from 'prop-types';
 
 class BucketView extends Component {
+  static propTypes = {
+    buckets: array,
+    style: object,
+  };
+
   render() {
+    const { buckets, style } = this.props;
+
     return (
-      <div>
-        Testing
+      <div className="bucket-view" style={style}>
+        {buckets.map(( bucket, i ) => (
+          <div className="bucket" key={bucket.title}>
+            <div className="bucket-title">
+              {bucket.title}
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
