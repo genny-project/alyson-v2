c86183ec31061298855409464692a160456082c1
matthaywardwebdesign <matt@matthayward.com.au>
2017-09-20 21:31:02 +1000

    Updated bucket view

--- client/views/components/bucket-view/BucketView.jsx
+++ client/views/components/bucket-view/BucketView.jsx
@@ -1,12 +1,27 @@
 import './bucketView.scss';
 import React, { Component } from 'react';
-import { array, object } from 'prop-types';
+import { array, object, any } from 'prop-types';
 
 class BucketView extends Component {
   static propTypes = {
     buckets: array,
     style: object,
+    children: any,
   };
+
+  renderBucket( i ) {
+    const { children } = this.props;
+    let childs = React.Children.toArray(children);
+    const bucketChildren = childs.filter(child => {
+      return child.props.index != undefined && child.props.index == i;
+    });
+
+    return (
+      <div key={i} className="bucket-contents">
+        {bucketChildren}
+      </div>
+    );
+  }
 
   render() {
     const { buckets, style } = this.props;
@@ -17,6 +32,9 @@ class BucketView extends Component {
           <div className="bucket" key={bucket.title}>
             <div className="bucket-title">
               {bucket.title}
+            </div>
+            <div className="bucket-contents">
+              {this.renderBucket( i )}
             </div>
           </div>
         ))}
