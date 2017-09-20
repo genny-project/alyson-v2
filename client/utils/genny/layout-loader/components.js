46ece4aa768e69a7cae75e431e2e5096aedec9b0
matthaywardwebdesign <matt@matthayward.com.au>
2017-09-20 22:04:07 +1000

    Updated layout

--- client/utils/genny/layout-loader/components.js
+++ client/utils/genny/layout-loader/components.js
@@ -1,10 +1,6 @@
 import { Grid, Repeater, ComponentCollection } from '@genny-project/layson';
-<<<<<<< HEAD
 import { BucketView, Sidebar, ImageView, TreeView } from 'views/components';
-=======
-import { BucketView, Sidebar, ImageView } from 'views/components';
 import { Card } from 'views/components/generic';
->>>>>>> 3cac374d03c946432cba6372363ec41ea3753288
 
 export default new ComponentCollection({
   Grid,
@@ -12,6 +8,6 @@ export default new ComponentCollection({
   BucketView,
   Sidebar,
   ImageView,
-  TreeView
+  TreeView,
   Card
 });
