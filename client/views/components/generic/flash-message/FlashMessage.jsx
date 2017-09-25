4f1c524ab3a7e907d30fbc396434a88b002d34b2
Nischal Gautam <nischal1024@gmail.com>
2017-09-25 14:52:50 +1000

    added flash message component

---
+++ client/views/components/generic/flash-message/FlashMessage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { string } from 'prop-types';
+import './flashmessage.scss';
+
+
+FlashMessage.propType = {
+  messageType: string,
+  title: string,
+  body: string
+};
+
+FlashMessage.defaultProps = {
+  messageType: 'primary',
+  title: 'Title Example',
+  body: 'Body Example'
+};
+
+function FlashMessage() {
+  return (
+    <div className="flash-message">
+      <div className={this.props.messageType} >
+        <h4 className="title"> {this.props.title} </h4>
+        <p>{this.props.body}</p>
+      </div>
+    </div>
+  );
+}
+
+export default FlashMessage;
