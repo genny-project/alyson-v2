d8ca4fe5d5ff75102fd20169d785a6d4addd9414
Nischal Gautam <nischal1024@gmail.com>
2017-09-25 12:37:27 +1000

    added custom icon component

--- client/epics/authLoggedIn.js
+++ client/epics/authLoggedIn.js
@@ -3,8 +3,8 @@ import { GennyBridge } from 'utils/genny';
 
 const authLoggedIn = action$ => {
   return action$
-    .ofType( AUTH_LOGGED_IN )
-    .do( action => GennyBridge.sendAuthInit( action.payload.token ))
+    .ofType(AUTH_LOGGED_IN)
+    .do(action => GennyBridge.sendAuthInit(action.payload.token))
     .mapTo({
       type: BRIDGE_SENT_AUTH_INIT,
     });
---
+++ client/views/components/generic/icon-custom/IconCustom.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './IconCustom.css';
+
+
+function IconCustom(props) {
+  return (
+    <div className="icon-custom">
+
+      {props.src ? <img src={this.props.src} /> : <i className='material-icons' style={{ fontSize: 90, color: '#b9b9b9' }}>photo_camera</i>}
+
+    </div>
+  );
+}
+export default IconCustom;
+
+
