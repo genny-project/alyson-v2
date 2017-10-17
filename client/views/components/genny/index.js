84567b1b906b8a978b3efb2eb7709fbf22458a99
Loris Campanile <loris1634@gmail.com>
2017-10-17 12:45:09 +1100

    fix sidebar

--- client/views/components/genny/index.js
+++ client/views/components/genny/index.js
@@ -2,3 +2,4 @@ export { default as AppHolder } from './app-holder';
 export { default as GennyForm } from './genny-form';
 export { default as GennyTable } from './genny-table';
 export { default as GennyTreeView } from './genny-tree-view';
+export { default as GennyImageView } from './genny-image-view';
