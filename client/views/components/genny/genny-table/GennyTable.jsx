daf9103cacffd06104b10528cf8073dfd2304d11
Loris Campanile <loris1634@gmail.com>
2017-10-20 11:58:07 +1100

    memberhub layout

--- client/views/components/genny/genny-table/GennyTable.jsx
+++ client/views/components/genny/genny-table/GennyTable.jsx
@@ -12,11 +12,43 @@ class GennyTable extends Component {
   }
 
   render() {
-    const {  } = this.props;
+
+    const { baseEntity, asks } = this.props;
+
+    // data
+    // columns
+
+    // name of the ask = name of column
+    // question.attributeCode = value to display
+    // { "firstName" : "4office", "lastName": "5photo", "age": 344, "visits": 13, "progress": 23, "status" : "tbool" }
+    // { "Header": "Last Name", "accessor": "lastName" }
+
+    let columns = [
+
+        {
+            "Header": "Attribute Code",
+            "accessor": "code"
+        },
+        {
+            "Header": "Attribute Value",
+            "accessor": "value"
+        }
+    ];
+
+    let data = [];
+    let be = baseEntity.data["PER_USER1"] || {}; 
+    let be_attributes = be.attributes || [];
+    be_attributes.forEach(attribute => {
+
+        data.push({
+            "code": attribute.code,
+            "value": attribute.value
+        });
+    });
 
     return (
       <div className="genny-table">
-        <Table {...this.props}/>
+        <Table {...this.props} data={data} columns={columns} />
       </div>
     );
   }
