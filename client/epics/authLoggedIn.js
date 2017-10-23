43ab025b0b4de613730b282d28fe654fa20978f0
Loris Campanile <loris1634@gmail.com>
2017-10-23 15:31:18 +1100

    fix localhost issue

--- client/epics/authLoggedIn.js
+++ client/epics/authLoggedIn.js
@@ -2,6 +2,7 @@ import { AUTH_LOGGED_IN, BRIDGE_SENT_AUTH_INIT } from 'constants/';
 import { GennyBridge } from 'utils/genny';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mapTo';
 
 const authLoggedIn = action$ => {
   return action$
--- client/epics/receiveKeycloakConfig.js
+++ client/epics/receiveKeycloakConfig.js
@@ -2,6 +2,7 @@ import { RECEIVE_KEYCLOAK_CONFIG, INIT_VERTX } from 'constants/';
 import { GennyBridge } from 'utils/genny';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mapTo';
 
 const receiveKeycloakConfig = action$ => {
   return action$
