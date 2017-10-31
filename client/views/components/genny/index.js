3a83184691d8e481421f8e8eb9b4e046108076d8
Conor Pirie <conor.pirie@gmail.com>
2017-11-01 10:09:31 +1100

    styling for card component

--- client/views/components/genny/index.js
+++ client/views/components/genny/index.js
@@ -1,4 +1,5 @@
 export { default as AppHolder } from './app-holder';
+export { default as GennyBucketView } from './genny-bucket-view';
 export { default as GennyForm } from './genny-form';
 export { default as GennyTable } from './genny-table';
 export { default as GennyTreeView } from './genny-tree-view';
