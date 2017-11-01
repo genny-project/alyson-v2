8a510806746c634cc08d945333755ea4b75296b5
Loris Campanile <loris1634@gmail.com>
2017-11-01 11:17:07 +1100

    bucket

--- client/views/components/genny/genny-bucket-view/GennyBucketView.jsx
+++ client/views/components/genny/genny-bucket-view/GennyBucketView.jsx
@@ -1,9 +1,7 @@
 import './gennyBucketView.scss';
 import React, { Component } from 'react';
-import { object, array } from 'prop-types';
 import BaseEntityQuery from './../../../../utils/genny/BaseEntityQuery';
-import { IconSmall, BucketView } from '../../';
-import { GennyBridge } from 'utils/genny';
+import { IconSmall, BucketView, Card } from '../../';
 
 class GennyBucketView extends Component {
 
@@ -11,18 +9,50 @@ class GennyBucketView extends Component {
     };
 
     state = {
+
+    }
+
+    generateBucket(query, group) {
+
+        // group = {code: GRP_NO_QUOTES, name: "No quotes"}
+        let groupCode = group.code;
+        return [(
+            <div>{groupCode}</div>
+        )]
+    }
+
+    generateBuckets(query, groups) {
+
+        // groups = [{code: GRP_NO_QUOTES, name: "No quotes"}, ....]
+        return groups.map(group => {
+
+            console.log("=========");
+            console.log(group);
+            return {
+                title: group.name
+            }
+        });
+    }
+
+    generateBucketView = (root) => {
+
+        let query = new BaseEntityQuery(this.props);
+        let rootGroups = query.getEntityChildren(root);
+        let buckets = rootGroups.map(groups => this.generateBucket(query, groups));
+
+        console.log(rootGroups);
+        console.log(buckets);
+        return buckets;
     }
 
     render() {
 
-        const { root, showBaseEntity } = this.props;
-
-        let query = new BaseEntityQuery(this.props);
-        //let children = query.getEntityChildren(root);
+        const { root } = this.props;
+        let buckets = this.generateBucketView(root);
 
         return (
             <div className="genny-bucket-view">
-                <BucketView {...this.props} />
+                <BucketView buckets={buckets} />
             </div>
         );
     }
