6938f63bd2d00182c90298046a75cf7c81b53e53
Conor Pirie <conor.pirie@gmail.com>
2017-11-02 17:39:20 +1100

    added pagination component to form

--- client/views/components/generic/input-date-picker/InputDatePicker.jsx
+++ client/views/components/generic/input-date-picker/InputDatePicker.jsx
@@ -2,43 +2,54 @@ import './inputDatePicker.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import React, { Component } from 'react';
-import { string, object, any } from 'prop-types';
+import { string, object, any, func } from 'prop-types';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import {  } from '../';
 
 class InputDatePicker extends Component {
 
   static defaultProps = {
-    className: {},
+    className: '',
+    defaultValue: '',
+    identifier: null,
+    validationStatus: null
   }
 
   static propTypes = {
-    className: object,
+    className: string,
     style: string,
     children: any,
+    defaultValue: string,
+    validation: func,
+    identifier: any,
+    validationStatus: string
   }
 
   state = {
-    startDate: '',
+    startDate: this.props.defaultValue ? this.props.defaultValue : '',
   }
 
   handleChange = (date) => {
-    this.setState({
-      startDate: date
-    });
+    const { validationList, validation, identifier } = this.props;
+    const value = moment(date).format("YYYY-MM-DD");
+    console.log(value);
+    this.setState({ startDate: date });
+    if(validation) validation(value, identifier, validationList);
   }
 
-
   render() {
- 	  const { className, children, style } = this.props;
-    const {  } = this.state;
+ 	  const { className, children, style, validationStatus } = this.props;
+    const { startDate } = this.state;
     const componentStyle = { ...style, };
 
     return (
       <div className={`input-date-picker ${className}`}>
         <DatePicker
-          className="start-date"
-          selected={this.state.startDate}
+          className={validationStatus}
+          calendarClassName="rasta-stripes"
+          dayClassName={date => date.date() < Math.random() * 31 ? 'random' : undefined}
+          selected={startDate}
           onChange={this.handleChange}
           peekNextMonth
           showMonthDropdown
