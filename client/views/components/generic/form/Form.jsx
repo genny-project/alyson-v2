cde2673c42dfc367e6277f6f2e878d1c2a7b0147
Conor Pirie <conor.pirie@gmail.com>
2017-11-24 16:48:27 +1100

    moved pagination logic from form and list to pagination component

--- client/views/components/generic/form/Form.jsx
+++ client/views/components/generic/form/Form.jsx
@@ -18,54 +18,28 @@ class Form extends Component {
 
   state = {
     showProgress: this.props.showProgress ? this.props.showProgress : false,
-    pageCount: Math.ceil( Object.keys(this.props.children).length / this.props.itemsPerPage ),
-    childrenCurrent: 1,
-    pageCurrent: 1,
-    offset: 0
   }
 
-  getChildrenCount = (childrenCount, itemsPerPage) => {
-    const arrChildren = [...Array(childrenCount).keys()].map(x => ++x);
-    const arrPage = [];
-    let arrPageConvert = arrChildren.map(child => {
-      arrPage.push({ child: child, page: Math.ceil(child/itemsPerPage) });
-    })
-    return arrPage;
-  }
-
-  getChildrenForCurrentPage = (itemsPerPage, offset, children) => {
-    let displayedItems = children.slice(offset, offset + itemsPerPage);
-    return displayedItems;
-  }
-
-  pageChange = (selectedPage) => {
-    const { itemsPerPage } = this.props;
-    let offset = Math.ceil(selectedPage * itemsPerPage);
-    this.setState({offset: offset, pageCurrent: selectedPage + 1}, () => {
-    });
-  }
 
   render() {
 
-    const { className, children, style, itemsPerPage, showProgress, isHorizontal } = this.props;
-    const { childrenCurrent, pageCurrent, pageCount, offset } = this.state;
+    const { className, children, style, itemsPerPage, showProgress, isHorizontal, hideNav } = this.props;
+    const { } = this.state;
     const componentStyle = { ...style, };
 
     let childrenCount = Object.keys(this.props.children).length;
-    const childrenPageArray = this.getChildrenForCurrentPage(itemsPerPage, offset, children);
 
     return (
       <div className={`form-container ${isHorizontal ? 'horizontal' : null }`}>
         <div className="form-main">
-          { showProgress && itemsPerPage <= childrenCount ? <ProgressBar progressTotal={pageCount} progressCurrent={pageCurrent} type={1} /> : null }
+          {/* showProgress && itemsPerPage <= childrenCount ? <ProgressBar progressTotal={pageCount} progressCurrent={pageCurrent} type={1} /> : null */}
           <div className="form-fields">
-  	        {childrenPageArray}
+            { !isHorizontal ?
+              <Pagination perPage={itemsPerPage} hideNav={hideNav} >
+                {children}
+              </Pagination>
+            : {children} }
           </div>
-          { !isHorizontal ?
-            <div className="form-nav">
-              <Pagination hidePageNumbers={itemsPerPage >= childrenCount} perPage={itemsPerPage} totalItems={childrenCount} pageChange={this.pageChange}/>
-            </div>
-          : null }
         </div>
       </div>
     );
