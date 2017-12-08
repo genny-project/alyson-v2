92ba2d7eb75603be745253af3aeb87c9aa0729fd
Loris Campanile <loris1634@gmail.com>
2017-12-08 11:21:48 +1100

    tree view and form

--- client/views/components/genny/app-content/appContent.container.js
+++ client/views/components/genny/app-content/appContent.container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import AppContent from './AppContent';
 
 function mapStateToProps(state) {
-  return { layout: state.layouts, baseEntity: state.baseEntity.data }
+  return { layout: state.layouts, baseEntity: state.baseEntity.data, asks: state.ask.data }
 }
 
 export default connect( mapStateToProps, null )( AppContent );
