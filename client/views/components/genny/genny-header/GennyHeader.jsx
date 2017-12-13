f412ddbead8292423fce530b05a745d6d0f04f40
Conor Pirie <conor.pirie@gmail.com>
2017-12-13 12:25:37 +1100

    fixed for mobile

--- client/views/components/genny/genny-header/GennyHeader.jsx
+++ client/views/components/genny/genny-header/GennyHeader.jsx
@@ -75,7 +75,7 @@ class GennyHeader extends Component {
           <Label text={projectTitle} position={[0,0]} />
           {/*<GennyNotification position={[0,1]} />*/}
           <Device isDesktop position={[0,1]}>
-            <Dropdown open
+            <Dropdown 
               style={{ marginRight: '30px'}} 
               header={
                 <span style={{display: 'flex', alignItems: 'center'}}><Label text={`${userName}`} /><IconSmall name="expand_more" /></span>}
