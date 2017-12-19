69266ef08bbbd50108112aa42be756ab840b8e94
Nischal Gautam <nischal1024@gmail.com>
2017-12-19 16:00:27 +1100

    added js for css-js-button

--- client/views/components/generic/button/Button.jsx
+++ client/views/components/generic/button/Button.jsx
@@ -24,16 +24,62 @@ class Button extends Component {
     const { onClick, handleClick } = this.props;
     
     if (handleClick) {
-      return handleClick
+      return handleClick;
     } else {
-      return onClick
+      return onClick;
     }
   }
 
   render() {
+
+    /* js stylesheet */
+
+    const buttonClass = {
+      height: 50,
+      width: '100%',
+      display: 'flex'
+    };
+
+    const buttonElement = {
+      width: 'inherit',
+      border: 'none',
+      borderRadius: 5,
+      color: '#fff',
+      backgroundColor: '#999',
+      cursor: 'pointer',
+      outline: 'none',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
+
+    const buttonConfirm = {
+      backgroundColor: '#5cb85c'
+    };
+
+    const buttonCancel = {
+      backgroundColor: '#cc0000'
+    };
+
+
+    const buttonSmall = {
+      height: 30,
+      fontSize: '1.5em'
+    };
+
+    const buttonSmallI = {
+      fontSize: '1.5em'
+    };
+
+    const buttomElementI = {
+      paddingRight: 5,
+      color: '#fff'
+    };
+
+    /* js stylesheet ends here */
+    
     const { children, type, className, href, onClick, style, color, buttonStyle } = this.props;
     const componentStyle = { ...style, };
-
     const clickFunc = this.getClickFunction();
 
     const btn = (
