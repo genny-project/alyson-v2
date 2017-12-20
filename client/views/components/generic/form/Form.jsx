4bf806d2b090bd68b82edf19a1f489d53c767b18
Nischal Gautam <nischal1024@gmail.com>
2017-12-20 15:10:30 +1100

    fixed form

--- client/views/components/generic/form/Form.jsx
+++ client/views/components/generic/form/Form.jsx
@@ -4,63 +4,6 @@ import { Pagination, Input } from 'views/components';
 import { string, bool, number, array} from 'prop-types';
 import { FormGroup } from './form-group';
 
-/* Forms  Stylesheet starts*/
-
-const formContainer = {
-  background: '#fff',
-  display: 'flex',
-  justifyContent: 'center',
-  flexGrow: 1
-};
-
-const formMain = {
-  color: '#222',
-  width: 600,
-  marginTop: 50,
-  marginBottom: 50, 
-  padding: 20, 
-};
-
-const formFields = {
-  paddingBottom: 10, 
-  };
-
-const formContentZorizontal = {
-  background: '#999',
-  borderRadius: 5,
-  marginBottom: 10, 
-};
-
-const HorizontalFormMain = {
-  width: '100%',
-  marginTop: 0,
-  marginBottom: 0, 
-  padding: 5
-};
-
-const horizontalFormFields = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  paddingBottom: 0,
-  flexWrap: 'wrap',
-};
-
-const inputText = {
-  marginBottom: '0 !important',
-  flexGrow: 1,
-  padding: 5
-};
-
-const input = {
-  paddingTop: 3,
-  paddingBottom: 3,
-  height: 40,
-  borderRadius: 10
-};
-
-
-/* Forms Stylesheet ends  */
-
 class Form extends Component {
 
   static defaultProps = {
@@ -84,7 +27,6 @@ class Form extends Component {
 
       if(Array.isArray( questionGroup )) {
         return questionGroup.map(group => {
-          
             if(group.content) return this.renderGroup(group);
             return group;
         });
@@ -92,30 +34,36 @@ class Form extends Component {
       else if (questionGroup.content) {
         return (<FormGroup title={questionGroup.title} data={this.renderGroup(questionGroup.content)}/>);
       }
-
       return [];
   }
 
   render() {
-
-    const { className, style, itemsPerPage, showProgress, isHorizontal, hideNav, data } = this.props;
-    const componentStyle = { ...style, };
-
-    let questionGroup = this.renderGroup( data );
-    return (
-      <div className={`form-container ${isHorizontal ? 'horizontal' : null }`} style={componentStyle}>
-        <div className="form-main">
-          <div className="form-fields">
-            { !isHorizontal && questionGroup.length > itemsPerPage ?
-              <Pagination perPage={itemsPerPage} hideNav={hideNav}>
-                {questionGroup}
-              </Pagination>
-            : questionGroup }
-          </div>
-        </div>
-      </div>
-    );
-  }
+             /* Forms  Stylesheet starts*/
+             const formContainer = { background: '#fff', display: 'flex', justifyContent: 'center', flexGrow: 1 };
+             const formMain = { color: '#222', width: 600, marginTop: 50, marginBottom: 50, padding:20 };
+             const formFields = { paddingBottom: 10 };
+             const formContainerHorizontal = { background: '#999', borderRadius: 5, marginBottom: 10 };
+             let   horizontalFormMain = null;
+             const horizontalFormFields = { display: 'flex', justifyContent: 'space-between', paddingBottom: 0, flexWrap: 'wrap' };
+             const FormFieldsInputText = { marginBottom: '0 !important', flexGrow: 1, padding: 5 };
+             const formFieldsInput = { paddingTop: 3, paddingBottom: 3, height: 40, borderRadius: 10 };
+            if(isHorizontal){
+              horizontalFormMain= { width: '100%', marginTop: 0, marginBottom: 0, padding: 5 };
+            }
+             /* Forms Stylesheet Ends  */
+             const { className, style, itemsPerPage, showProgress, isHorizontal, hideNav, data } = this.props;
+             const componentStyle = { ...style };
+             let questionGroup = this.renderGroup(data);
+             return <div style={{...formContainer,...formContainerHorizontal}}>
+                 <div style={formMain}>
+                   <div style={formFields}>
+                     {!isHorizontal && questionGroup.length > itemsPerPage ? <Pagination perPage={itemsPerPage} hideNav={hideNav}>
+                         {questionGroup}
+                       </Pagination> : questionGroup}
+                   </div>
+                 </div>
+               </div>;
+           }
 }
 
 export default Form;
