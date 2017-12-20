07746ac17bae14bbc20ee1d5bdb4ddf5b5237321
Nischal Gautam <nischal1024@gmail.com>
2017-12-20 15:27:31 +1100

    removed unnecessary css files

--- client/views/components/generic/form/Form.jsx
+++ client/views/components/generic/form/Form.jsx
@@ -1,4 +1,4 @@
-import './form.scss';
+// import './form.scss';
 import React, { Component } from 'react';
 import { Pagination, Input } from 'views/components';
 import { string, bool, number, array} from 'prop-types';
@@ -39,24 +39,17 @@ class Form extends Component {
 
   render() {
              /* Forms  Stylesheet starts*/
-             const formContainer = { background: '#fff', display: 'flex', justifyContent: 'center', flexGrow: 1 };
+             const formContainer = { backgroundColor: '#fff', display: 'flex', justifyContent: 'center', flexGrow: 1 };
              const formMain = { color: '#222', width: 600, marginTop: 50, marginBottom: 50, padding:20 };
              const formFields = { paddingBottom: 10 };
-             const formContainerHorizontal = { background: '#999', borderRadius: 5, marginBottom: 10 };
-             let   horizontalFormMain = null;
-             const horizontalFormFields = { display: 'flex', justifyContent: 'space-between', paddingBottom: 0, flexWrap: 'wrap' };
-             const FormFieldsInputText = { marginBottom: '0 !important', flexGrow: 1, padding: 5 };
-             const formFieldsInput = { paddingTop: 3, paddingBottom: 3, height: 40, borderRadius: 10 };
-            if(isHorizontal){
-              horizontalFormMain= { width: '100%', marginTop: 0, marginBottom: 0, padding: 5 };
-            }
              /* Forms Stylesheet Ends  */
+             
              const { className, style, itemsPerPage, showProgress, isHorizontal, hideNav, data } = this.props;
              const componentStyle = { ...style };
              let questionGroup = this.renderGroup(data);
-             return <div style={{...formContainer,...formContainerHorizontal}}>
-                 <div style={formMain}>
-                   <div style={formFields}>
+             return <div className={`form-container ${isHorizontal ? 'horizontal' : null}`} style={{ ...formContainer }}>
+                 <div className="form-main" style={formMain}>
+                   <div className="form-fields" style={formFields}>
                      {!isHorizontal && questionGroup.length > itemsPerPage ? <Pagination perPage={itemsPerPage} hideNav={hideNav}>
                          {questionGroup}
                        </Pagination> : questionGroup}
