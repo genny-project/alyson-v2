fb353655964482d056defaadfec7acd48efede2a
Nischal Gautam <nischal1024@gmail.com>
2017-12-20 17:43:47 +1100

    some changes bug fix needed

--- client/views/components/generic/breadcrumbs/Breadcrumbs.jsx
+++ client/views/components/generic/breadcrumbs/Breadcrumbs.jsx
@@ -1,4 +1,4 @@
-import './breadcrumbs.scss';
+// import './breadcrumbs.scss';
 import React, { Component } from 'react';
 import { string, object } from 'prop-types';
 import { IconSmall } from '../';
@@ -23,7 +23,7 @@ class Breadcrumbs extends Component {
     let stringPath = this.props.currentPath;
     if(stringPath && stringPath.length > 0) {
 
-        if(stringPath[stringPath.length - 1] == "/") {
+        if(stringPath[stringPath.length - 1] == '/') {
             stringPath = stringPath.slice(0, -1);
         }
 
@@ -38,7 +38,7 @@ class Breadcrumbs extends Component {
                         <IconSmall name='chevron_right' />
                         <span>{path}</span>
                     </li>
-                )
+                );
             }
         });
     }
@@ -47,23 +47,28 @@ class Breadcrumbs extends Component {
   }
 
   render() {
+             /* Styles start */
+             const breadcrumbsCss = { height: 40, width: '100%', display: 'flex', alignItems: 'center' };
+             const breadcrumbsMain = { display: 'flex', marginTop: 0, marginBottom: 0, padding: 0 };
+             const li = { display: 'flex', alignItems: 'center', paddingRight: 10, cursor: 'pointer' };
+             const span = { fontSize: '0.8em' };
+             const iEle = { padding: 0, color: 'white' };
+             /* Styles ends here */
 
-    const { className, style, home } = this.props;
-    const componentStyle = { ...style, };
-    const breadcrumbs = this.createBreadcrumbs();
+             const { className, style, home } = this.props;
+             const componentStyle = { ...style };
+             const breadcrumbs = this.createBreadcrumbs();
 
-    return (
-      <div className={`breadcrumbs ${className}`} style={componentStyle}>
-        <ul className='breadcrumbs-main'>
-          <li className='breadcrumbs-home'>
-            <IconSmall name='home' />
-            <span>Home</span>
-          </li>
-          {breadcrumbs}
-        </ul>
-      </div>
-    );
-  }
+             return <div className={`breadcrumbs ${className}`} style={{ ...breadcrumbsCss, ...componentStyle }}>
+                 <ul className="breadcrumbs-main" style={{ ...breadcrumbsMain, ... {listStyleType:'none' }}}>
+                   <li className="breadcrumbs-home" style={{...li}}>
+                     <IconSmall name="home" style={iEle}/>
+                     <span style={{...span}}>Home</span>
+                   </li>
+                   {breadcrumbs}
+                 </ul>
+               </div>;
+           }
 }
 
 export default Breadcrumbs;
