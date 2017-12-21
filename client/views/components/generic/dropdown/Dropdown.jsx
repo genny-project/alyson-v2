afdb8ec2f6eedc1dd2ca21c1e81cffd441471994
Nischal Gautam <nischal1024@gmail.com>
2017-12-21 16:36:37 +1100

    added styles

--- client/views/components/generic/dropdown/Dropdown.jsx
+++ client/views/components/generic/dropdown/Dropdown.jsx
@@ -4,11 +4,8 @@ import { string, any, bool, object } from 'prop-types';
 import components from 'utils/genny/layout-loader/components.js';
 import { JSONLoader } from '@genny-project/layson';
 
-/* Stylesheet */
 
 
-
-/* stylesheet ends her */
 class Dropdown extends Component {
   static defaultProps = {
     className: '',
@@ -61,24 +58,27 @@ class Dropdown extends Component {
   }
 
   render() {
-    const { className, children, style, contentStyle, tagStyle, header, open, noDropdownStyle, showTag, inline,  } = this.props;
-    let { isOpen, } = this.state;
-    if(open != undefined) isOpen = open; // open props overrides
+             /* Stylesheet */
+             const dropdown = { display: 'flex', alignItems: 'center', flexDirection: 'column', cursor: 'pointer', position: 'relative' };
+             const dropdownHeader = { zIndex: 9 };
+             const dropdownContent = { position: 'absolute', zIndex: 10, top: 40, left: 50, transform: 'translate(-50%)', padding: '10px 0 20px', margin: 0 };
+             const inlineDropdownContent = { position: 'initial', transform: 'initial', padding: 10, width: '100%' };
+             const dropdownTag = { height: 0, borderStyle: 'solid', borderWidth: '0px 9px 10px', borderColor: 'transparent transparent #DDD', position: 'absolute',top: '-10px',left: '50%', transform: 'translate(-50%)'  };
+             /* Stylesheet Ends here */
 
-    return (
-      <div className={`dropdown ${className} ${ inline ? 'inline' : '' }`} onClick={this.handleClick} onBlur={this.handleBlur}  tabIndex='-1' style={style} >
-        <div className='dropdown-header'>
-          {this.renderHeader()}
-        </div>
-        { isOpen ?
-          <div className={`dropdown-content ${noDropdownStyle ? 'no-style' : ''}`} style={contentStyle} >
-            { showTag && !inline ? <div className='dropdown-tag' style={tagStyle}></div> : null }
-            {children}
-          </div>
-        : null }
-      </div>
-    );
-  }
+             const { className, children, style, contentStyle, tagStyle, header, open, noDropdownStyle, showTag, inline } = this.props;
+             let { isOpen } = this.state;
+             if (open != undefined) isOpen = open; // open props overrides
+             return <div className={`dropdown ${className} ${inline ? 'inline' : ''}`} onClick={this.handleClick} onBlur={this.handleBlur} tabIndex="-1" style={{...dropdown,...style}}>
+                 <div className="dropdown-header" style={{...dropdownHeader}}>
+                   {this.renderHeader()}
+                 </div>
+                 {isOpen ? <div className={`dropdown-content ${noDropdownStyle ? 'no-style' : ''}`} style={{...dropdownContent,...contentStyle}}>
+                     {showTag && !inline ? <div className="dropdown-tag" style={tagStyle} /> : null}
+                     {children}
+                   </div> : null}
+               </div>;
+           }
 }
 
 export default Dropdown;
