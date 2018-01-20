2c694ef99bbd723177cc4640a04961560d4e7ffb
zcallan <kaln.delbridge@hotmail.com>
2018-01-20 16:19:02 +1100

    updated genny router to work for layouts

--- client/utils/genny/router/gennyRouter.middleware.js
+++ client/utils/genny/router/gennyRouter.middleware.js
@@ -1,21 +1,40 @@
+import { CMD_VIEW_PAGE_CHANGE, CMD_VIEW } from 'constants';
 import { push } from 'react-router-redux';
 
 
 const middleware = store => next => action => {
-  if ( action.type === 'CMD_VIEW' ) {
-    console.log( '==== NEW VIEW ====' );
-
+  /* Capture actions that change the view. */
+  if ( action.type === CMD_VIEW ) {
+    /* Create a new object out of the payload and delete the token from it. */
     const layout = { ...action.payload };
     delete layout.token;
 
+    /* Stringify the payload so it can go into the page URL. */
     const stringifiedLayout = JSON.stringify( layout );
-    console.log( stringifiedLayout );
 
-    // store.dispatch( push( stringifiedLayout ));
+    /* Dispatch a push to the browser history so that the layout is saved in the URL. */
+    store.dispatch( push( stringifiedLayout ));
   }
 
+  /* Capture when the page changes. */
   else if ( action.type === '@@router/LOCATION_CHANGE' ) {
-    console.log( 'location change', action.payload );
+    try {
+      /* Get the stringified layout from the payload, which is ultimately from the URL. */
+      const stringifiedLayout = action.payload.pathname.replace( '/', '' );
+      /* Convert the layout from the URL to JSON so we can reconstruct the original CMD_VIEW payload. */
+      const layout = JSON.parse( stringifiedLayout );
+
+      /* Dispatch an altered CMD_VIEW action with the layout. The reason it is altered
+       * is so that the above function (the one that pushes the layout the URL) does not
+       * get triggered and send us into an infinite loop of pushing and page changing. */
+      store.dispatch({
+        type: CMD_VIEW_PAGE_CHANGE,
+        payload: layout,
+      });
+    } catch ( e ) {
+      /* Handle failed `JSON.parse`s. */
+      console.error( e );
+    }
   }
 
   next( action );
