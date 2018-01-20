93ca6d9282be636db3d0d1c7e50f102dfafa0c9f
matthaywardwebdesign <matt@matthayward.com.au>
2018-01-20 12:44:44 +1100

    Working on image upload

--- client/views/components/generic/input-upload/InputUpload.jsx
+++ client/views/components/generic/input-upload/InputUpload.jsx
@@ -1,88 +1,237 @@
 import './InputUpload.scss';
+import { string, bool, number, func, object, array } from 'prop-types';
 import React, { Component } from 'react';
-import { string, bool, array, any, func } from 'prop-types';
-import { Label, SubmitStatusIcon } from 'views/components';
-import { Grid } from '@genny-project/layson';
+import Uppy from 'uppy/lib/core';
+import AwsS3 from 'uppy/lib/plugins/AwsS3';
+import Webcam from 'uppy/lib/plugins/Webcam';
+import Dashboard from 'uppy/lib/plugins/Dashboard';
+import prettierBytes from 'prettier-bytes';
+import classNames from 'classnames';
 
 class InputUpload extends Component {
-
   static defaultProps = {
     className: '',
-    validationList: [],
-    mask: '',
-    name: '',
-    placeholder: '',
-    defaultValue: '',
-    readOnly: false,
-    optional: false,
-    identifier: null,
-    validationStatus: null
+    maxNumberOfFiles: 0,
+    autoProceed: true,
+    icon: 'add',
+    defaultValue: [],
   }
 
   static propTypes = {
     className: string,
-    validationList: array,
-    mask: string,
-    name: string,
-    placeholder: string,
-    defaultValue: string,
-    readOnly: bool,
-    optional: bool,
-    validation: func,
-    identifier: any,
-    validationStatus: string,
-    mandatory: bool,
-    isHorizontal: bool,
-    value: string,
+    maxNumberOfFiles: number,
+    icon: string,
+    autoProceed: bool,
+    showPhotoGalleryWithIndex: func,
+    onChange: func,
+    defaultValue: object,
+    value: array,
+    label: string,
   }
 
   state = {
-    date: new Date(),
-    hasChanges: false,
-    value: this.props.value || '',
-    focused: false,
+    files: this.props.value || this.props.defaultValue,
+    error: null,
   }
 
-  handleChange = event => {
-    // we consider for now we can only upload 1 file at a time. this could change.
-    if(event.target.files && event.target.files.length > 0) {
+  componentWillMount() {
+    const { autoProceed } = this.props;
 
-        let uploadedfile = event.target.files[0];
-        var reader  = new FileReader();
+    this.uppy = new Uppy({
+      autoProceed,
+      debug: process.env.PLEASED_ENVIRONMENT === 'development',
+      restrictions: {
+        maxNumberOfFiles: this.props.maxNumberOfFiles,
+      },
+    })
+      .use( Dashboard, {
+        closeModalOnClickOutside: true,
+      })
+      .use( AwsS3, { host: 'http://localhost:3020' })
+      .use( Webcam, { target: Dashboard })
+      .run();
 
-        reader.readAsDataURL(uploadedfile);
-        reader.onloadend = (event) => {
+    this.uppy.on( 'complete', this.handleComplete );
+  }
 
-            // base64
-            let base64String = event.target.result;
+  componentWillUnmount() {
+    this.uppy.close();
 
-            const { validationList, validation, identifier } = this.props;
-            if(validation) validation(base64String, identifier, validationList);
+    removeEventListener( 'hashchange', this.handleHashChange, false );
+  }
 
-            this.setState({
-              value: uploadedfile.name
-            });
-        };
+  get modalName() {
+    return 'uppy';
+  }
+
+  getIconByFileType = fileType => {
+    if ( fileType.includes( 'image' ))
+      return 'image';
+
+    if ( fileType.includes( 'video' ))
+      return 'videocam';
+
+    if ( fileType.includes( 'audio' ))
+      return 'audiotrack';
+
+    if ( fileType.includes( 'pdf' ))
+      return 'picture_as_pdf';
+
+    return 'insert_drive_file';
+  }
+
+  handleComplete = result => {
+    console.log( result );
+
+    this.setState( state => ({
+      files: [
+        ...state.files,
+        ...result.successful.map( file => ({
+          ...file,
+          uploaded: false,
+          id: file.meta.key,
+        })),
+      ],
+    }), this.handleSaveToServer );
+  }
+
+  handleSaveToServer = () => {
+    const { files } = this.state;
+
+    this.setState({ error: null });
+
+    const restructuredFiles = files.map( file => ({
+      id: file.id,
+      key: file.meta.key,
+      name: file.name,
+      type: file.type,
+    }));
+
+    console.log( restructuredFiles );
+  }
+
+  handleSuccess = success => {
+    console.log( success );
+
+    const uploadedFiles = success.response.map(({ id }) => id );
+
+    /* Update all the  */
+    this.setState( state => ({
+      files: [
+        ...state.files.filter(({ id }) => !uploadedFiles.includes( id )),
+        success.response,
+      ],
+    }), () => {
+      if ( this.props.onChange ) {
+        this.props.onChange({ target: { value: this.state.files } });
+      }
+    });
+  }
+
+  handleError = error => {
+    console.error( error );
+
+    this.setState({ error });
+  }
+
+  handleOpenModal = () => {
+    this.uppy.getPlugin( 'Dashboard' ).openModal();
+
+    /* Append some text in the location hash so that when the user
+     * navigates backwards in browser history, the modal closes. */
+    if ( !window.location.hash.includes( this.modalName )) {
+      if ( window.location.hash ) {
+        window.location.hash += `,${this.modalName}`;
+      } else {
+        window.location.hash = this.modalName;
+      }
+    }
+
+    /* Listen for if the user presses the back button. */
+    addEventListener( 'hashchange', this.handleHashChange, false );
+  }
+
+  handleHashChange = () => {
+    /* If the location hash no longer contains our text, the user has
+     * pressed back in their browser and we should close the modal. */
+    if ( !window.location.hash.includes( this.modalName )) {
+      this.uppy.getPlugin( 'Dashboard' ).closeModal();
+
+      /* Clean up the event listener. */
+      removeEventListener( 'hashchange', this.handleHashChange, false );
     }
   }
 
-  render() {
-    const { className, name, mandatory, placeholder, validationStatus, isHorizontal } = this.props;
+  handleRemoveFile = fileId => () => {
+    this.setState( state => ({ files: state.files.filter(({ id }) => id !== fileId ) }), this.handleRefreshUppy );
+  }
 
-    return <div className={`input input-file-upload ${className} ${validationStatus || ''}`}>
-      {!isHorizontal ? <div className="input-header">
-          {name ? <Label text={name} /> : null}
-          {mandatory ? <Label className='input-label-required' textStyle={{color: '#cc0000'}} text="*  required" /> : null}
-          <SubmitStatusIcon status={validationStatus} />
-        </div> : null}
-      <Grid rows={1} cols={[{ style: { flexGrow: 4, marginRight: '10px' }},1]}>
-        <input position={[0,0]} className="file-name-display" type='text' readOnly placeholder={placeholder || 'No file chosen'} value={this.props.value} />
-        <div position={[0,1]} className="fileUpload">
-          <span>+ Upload</span>
-          <input className="upload" id='fileUpload' type='file' onChange={this.handleChange} />
-        </div>
-      </Grid>
-    </div>;
+  handleRefreshUppy = () => {
+    this.uppy.setState({
+      files: this.state.files,
+    });
+  }
+
+  handleShowImage = imageToShow => event => {
+    event.preventDefault();
+
+    const images = this.state.files.filter(({ type }) => type.includes( 'image' ));
+    const index = images.findIndex( image => (( image.uploadURL === imageToShow.uploadURL ) || ( image.url === imageToShow.url )));
+
+    this.props.showPhotoGalleryWithIndex( index, ...images );
+
+    return false;
+  }
+
+  render() {
+    const { className, icon, label } = this.props;
+    const { files, error } = this.state;
+
+    return (
+      <div className={classNames( 'input', 'input-file', className, {})}>
+        {label && <label>{label}</label>}
+
+        {files.length > 0 && (
+          files.map( file => {
+            return (
+              <article key={file.id}>
+                <button type="button" onClick={this.handleRemoveFile( file.id )}>
+                  <i className="material-icons">close</i>
+                </button>
+
+                {( file.type.includes( 'image' ) && !!file.preview ) ? (
+                  <img src={file.preview} role="presentation" onClick={this.handleShowImage( file )} />
+                ) : (
+                  <aside>
+                    <i className="material-icons">{this.getIconByFileType( file.type )}</i>
+                  </aside>
+                )}
+
+                <div>
+                  <a
+                    href={file.uploadURL}
+                    target="_blank"
+                    rel="noopener"
+                    onClick={file.type.includes( 'image' ) && this.handleShowImage( file )}
+                  >
+                    {file.name} {file.uploaded ? ' (uploaded' : ' (not uploaded)'} {error && '(ERROR)'}
+                  </a>
+                  <small>{prettierBytes( file.size )}</small>
+                </div>
+              </article>
+            );
+          })
+        )}
+
+        <button type="button" onClick={this.handleOpenModal}>
+          <div>
+            <i className="material-icons">{icon}</i>
+          </div>
+
+          <p>Upload a{files.length > 0 && 'nother'} file or image</p>
+        </button>
+      </div>
+    );
   }
 }
 
