4fc4663b4cc73c833278402829de4f40823fa11c
Conor Pirie <conor.pirie@gmail.com>
2018-01-21 12:56:34 +1100

    updated selector to render arrays of children

--- client/views/components/generic/selector/Selector.jsx
+++ client/views/components/generic/selector/Selector.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { any, bool } from 'prop-types';
 
 class Selector extends Component {
@@ -13,9 +13,19 @@ class Selector extends Component {
   state = {
   }
 
+  renderChildren() {
+
+    const { children } = this.props;
+    if(children.constructor === Array) {
+      return <div>{children}</div>;
+    }
+
+    return children;
+  }
+
   render() {
 
-    const { checkValues, showValues, hideValues, showOverride, children } = this.props;
+    const { checkValues, showValues, hideValues, showOverride } = this.props;
     // checkValues : the values that will be compared to other values
     // showValues : array of values. if checkValues matches any of these values, return true
     // hideValues : array of values. if checkValues matches any of these values, return false
@@ -31,8 +41,8 @@ class Selector extends Component {
       if ( hideValues && hideValues == checkValues && !showOverride) {
         return null;
       }
-      return children;
-
+      return this.renderChildren();
+      
     } else {
 
         for (var i = 0; i < checkValues.length; i++) {
@@ -40,7 +50,7 @@ class Selector extends Component {
             const value = checkValues[i];
 
             if( showValues && showValues.includes(value)) {
-                return children;
+              return this.renderChildren();
             }
         }
 
