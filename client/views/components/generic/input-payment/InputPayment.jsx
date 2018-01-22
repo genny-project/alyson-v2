e90220b29350572209db52b944c837632da6dd9d
matthaywardwebdesign <matt@matthayward.com.au>
2018-01-22 14:36:50 +1100

    Working on payment input

--- client/views/components/generic/input-payment/InputPayment.jsx
+++ client/views/components/generic/input-payment/InputPayment.jsx
@@ -5,6 +5,19 @@ import PaymentType from './payment-type';
 class InputPayment extends Component {
   state = {
     selectedPaymentType: null,
+    stage: 0,
+  };
+
+  static defaultProps = {
+    accounts: [
+      {
+        id: 1,
+        type: 'BANK_ACCOUNT',
+        name: 'NAB Business',
+        bsb: '833023',
+        accountNumber: '126456432'
+      }
+    ]
   };
 
   onSelectPaymentType = type => () => {
@@ -17,18 +30,43 @@ class InputPayment extends Component {
     return this.state.selectedPaymentType != null;
   }
 
-  render() {
+  onGoNext = () => {
+    this.setState({
+      stage: this.state.stage + 1,
+    });
+  }
+
+  renderSelectType() {
     const { selectedPaymentType } = this.state;
 
     return (
-      <div className='input-payment'>
+      <div>
         <h2>Select payment type</h2>
         <p>Please select a payment type from below</p>
         <div className='payment-type-select'>
-          <PaymentType type='BANK ACCOUNT' selected={selectedPaymentType === 'BANK ACCOUNT'} icon='account_balance' onClick={this.onSelectPaymentType( 'BANK ACCOUNT' )} />
+          <PaymentType type='BANK ACCOUNT' selected={selectedPaymentType === 'BANK_ACCOUNT'} icon='account_balance' onClick={this.onSelectPaymentType( 'BANK_ACCOUNT' )} />
           <PaymentType type='CARD' selected={selectedPaymentType === 'CARD'} icon='credit_card' onClick={this.onSelectPaymentType( 'CARD' )} />
         </div>
-        <div className={`next-step-btn ${!this.isPaymentTypeSelected() ? 'disabled' : ''}`}>
+      </div>
+    );
+  }
+
+  renderSelectAccount() {
+    return (
+      <div>
+        <h2>Select account</h2>
+        <p>Please select a account from below</p>
+      </div>
+    );
+  }
+
+  render() {
+    const { stage } = this.state;
+    return (
+      <div className='input-payment'>
+        { stage === 0 && this.renderSelectType() }
+        { stage === 1 && this.renderSelectAccount() }
+        <div className={`next-step-btn ${!this.isPaymentTypeSelected() ? 'disabled' : ''}`} onClick={this.onGoNext}>
           <span>NEXT</span>
           <i className='material-icons'>chevron_right</i>
         </div>
