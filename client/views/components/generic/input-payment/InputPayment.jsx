90c2e0890346251535ad892733b923cc852dba10
matthaywardwebdesign <matt@matthayward.com.au>
2018-01-30 18:54:14 +1100

    Working on payment UI

--- client/views/components/generic/input-payment/InputPayment.jsx
+++ client/views/components/generic/input-payment/InputPayment.jsx
@@ -51,10 +51,13 @@ class InputPayment extends Component {
 
   componentDidMount() {
     const user = GennyBridge.getUser();
+    const project = GennyBridge.getProject ? GennyBridge.getProject() : null;
     const bankTokenAttribute = BaseEntityQuery.getBaseEntityAttribute( user, 'PRI_ASSEMBLY_BANK_TOKEN' );
     const cardTokenAttribute = BaseEntityQuery.getBaseEntityAttribute( user, 'PRI_ASSEMBLY_CARD_TOKEN' );
     const accountsAttribute = BaseEntityQuery.getBaseEntityAttribute( user, 'PRI_PAYMENT_METHODS' );
     const amount = this.props.data ? BaseEntityQuery.getBaseEntityAttribute( this.props.data.targetCode, 'PRI_OWNER_INCGST' ) : null;
+    const descriptionAttribute = BaseEntityQuery.getBaseEntityAttribute( project, 'PRI_PAYMENT_CONFIRM_TEXT' );
+    const description = descriptionAttribute ? descriptionAttribute.value : 'will be charged / debited from the below account to escrow for the payment of this job.';
 
     this.setState({
       tokens: {
@@ -62,8 +65,9 @@ class InputPayment extends Component {
         bank: bankTokenAttribute ? bankTokenAttribute.value : null,
         card: cardTokenAttribute ? cardTokenAttribute.value : null,
       },
-      amount: amount ? amount.value : ( this.props.amount ? this.props.amount :  null ),
+      amount: amount ? amount.value : ( this.props.amount ? this.props.amount :  '0.00' ),
       accounts: accountsAttribute ? JSON.parse(accountsAttribute.value) : ( this.props.accounts ? this.props.accounts : []),
+      confirmDescription: description,
     });
 
     /* Generate a device ID and grab IP address for the user */
@@ -107,7 +111,11 @@ class InputPayment extends Component {
   }
 
   onGoNext = () => {
-    if ( this.state.stage === 1 ) {
+    if ( !this.canGoNext()) {
+      return;
+    }
+
+    if ( this.state.stage === 2 ) {
       return;
     }
 
@@ -131,6 +139,10 @@ class InputPayment extends Component {
     this.setState({
       stage: this.state.stage - 1,
     });
+  }
+
+  onHandleDone = () => {
+    alert( 'Done' );
   }
 
   handleSelectPaymentMethod = account => () => {
@@ -176,6 +188,33 @@ class InputPayment extends Component {
     );
   }
 
+  renderConfirmPayment() {
+    const { amount, confirmDescription, selectedPaymentMethod, accounts } = this.state;
+    const account = accounts.find( a => a.id === selectedPaymentMethod );
+    return (
+      <div>
+        <h2>{this.renderBackButton()} Confirm payment</h2>
+        <p>Click CONFIRM below to confirm that <b>${amount}</b> (including GST) {confirmDescription}</p>
+        <PaymentMethod
+          key={account.id}
+          account={account}
+          onClick={this.handleSelectPaymentMethod( account )}
+          selected={selectedPaymentMethod === account.id}
+        />
+      </div>
+    );
+  }
+
+  renderConfirmButton() {
+    const { amount } = this.state;
+    return (
+      <div className='confirm-btn' onClick={this.onHandleDone}>
+        <span>CONFIRM PAYMENT OF ${amount}</span>
+        <i className='material-icons'>check</i>
+      </div>
+    );
+  }
+
   renderNextButton() {
     return (
       <div className={`next-step-btn ${!this.canGoNext() ? 'disabled' : ''}`} onClick={this.onGoNext}>
@@ -208,7 +247,8 @@ class InputPayment extends Component {
       <div className='input-payment'>
         { stage === 0 && this.renderSelectType() }
         { stage === 1 && this.renderSelectAccount() }
-        {this.renderNextButton()}
+        { stage === 2 && this.renderConfirmPayment() }
+        { stage < 2 ? this.renderNextButton() : this.renderConfirmButton() }
       </div>
     );
   }
