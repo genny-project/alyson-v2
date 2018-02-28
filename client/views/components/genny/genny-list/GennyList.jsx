64ae56c18ca38a3c8b2056f76971af17f8ffe939
Conor Pirie <conor.pirie@gmail.com>
2018-02-28 15:18:21 +1100

    update

--- client/views/components/genny/genny-list/GennyList.jsx
+++ client/views/components/genny/genny-list/GennyList.jsx
@@ -62,6 +62,8 @@ class GennyList extends Component {
         const { root, showLinks, headerRoot, hideHeader, hideLinks, showTitle, showEmpty, gennyListStyle, ...rest } = this.props;
         const componentStyle = { ...gennyListStyle};
 
+        console.log(componentStyle);
+        
         const data = showLinks ? BaseEntityQuery.getBaseEntitiesForLinkCode(root, hideLinks) : BaseEntityQuery.getEntityChildren(root);
 
         const rootEntity = BaseEntityQuery.getBaseEntity(root);
