cd50edf88b4d014f8e478330bc222bde1d8e1f40
Conor Pirie <conor.pirie@gmail.com>
2018-03-06 12:03:08 +1100

    input

--- client/views/components/generic/input/Input.jsx
+++ client/views/components/generic/input/Input.jsx
@@ -404,14 +404,21 @@ class Input extends Component {
             );
             default:
             return (
-                <InputText
+                // <InputText
+                //     {...rest}
+                //     validation={this.validateInput}
+                //     validationStatus={validationStatus}
+                //     handleOnChange={this.handleOnChange}
+                //     value={this.state.value}
+                //     onFocus={this.onFocus}
+                //     onBlur={this.onBlur}
+                // />
+                <InputRating
                     {...rest}
                     validation={this.validateInput}
                     validationStatus={validationStatus}
                     handleOnChange={this.handleOnChange}
-                    value={this.state.value}
-                    onFocus={this.onFocus}
-                    onBlur={this.onBlur}
+                    //value={this.state.value}
                 />
             );
         }
