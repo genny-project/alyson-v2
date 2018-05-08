efec1c24b5f0e73e6936a554f1585fefba12e6ea
Conor Pirie <conor.pirie@gmail.com>
2018-05-08 10:29:34 +1000

    date label now converts utc to local to display dates and datetimes

--- client/views/components/generic/date-label/DateLabel.jsx
+++ client/views/components/generic/date-label/DateLabel.jsx
@@ -6,66 +6,69 @@ import { BaseEntityQuery } from 'utils/genny';
 
 class DateLabel extends Component {
 
-  static defaultProps = {
-    children: '',
-    format: 'DD/MM/YYYY',
-    fromNow: false,
-  }
-
-  static propTypes = {
-    className: string,
-    date: string,
-    format: string,
-    children: any,
-    style: object,
-    fromNow: bool,
-  }
-
-  renderDate = (date) => {
-    const { format } = this.props;
-
-    if (date) {
-      const formattedDate = moment(date).utcOffset(-4);
-      if(formattedDate.isValid()) return formattedDate.format(format);
+    static defaultProps = {
+        children: '',
+        format: 'DD/MM/YYYY',
+        fromNow: false,
     }
-    
-    return null;
-  }
 
-  renderFromNow = (code) => {
+    static propTypes = {
+        className: string,
+        date: string,
+        format: string,
+        children: any,
+        style: object,
+        fromNow: bool,
+    }
 
-    if (code) {
-      let createdAt = BaseEntityQuery.getBaseEntity(code);
-      if (createdAt && createdAt.created){
-        let date = moment(createdAt.created);
-        let now = moment();
-        return date.from(now);
-      }
-      else {
+    renderDate = (date) => {
+        const { format } = this.props;
+        if (date) {
+            let utc = date;
+            
+            const formattedDate = moment.utc(utc).local();
+            
+            if(formattedDate.isValid()) {
+                return formattedDate.format(format);
+            }
+        }
         return null;
-      }
-    } else {
-      return null;
     }
-  }
 
-  render() {
+    renderFromNow = (code) => {
 
-    const { className, fromNow, children, date, style } = this.props;
-    const componentStyle = { ...style, };
-    
-    return (
-      <div className={`date-label ${className || ''}`} style={componentStyle}>
-        <span className="date-label-text">
-          {
-            fromNow ?
-            this.renderFromNow(children || date) :
-            this.renderDate(children || date)
-          }
-        </span>
-      </div>
-    );
-  }
+        if (code) {
+            let createdAt = BaseEntityQuery.getBaseEntity(code);
+            if (createdAt && createdAt.created){
+                let date = moment(createdAt.created);
+                let now = moment();
+                return date.from(now);
+            }
+            else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+
+        const { className, fromNow, children, date, style } = this.props;
+        const componentStyle = { ...style, };
+        
+        return (
+        <div className={`date-label ${className || ''}`} style={componentStyle}>
+            <span className="date-label-text">
+                {
+                    fromNow ?
+                    this.renderFromNow(children || date) :
+                    this.renderDate(children || date)
+                }
+            </span>
+        </div>
+        );
+    }
 }
 
 export default DateLabel;
