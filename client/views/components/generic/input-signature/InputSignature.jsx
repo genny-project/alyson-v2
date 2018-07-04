0922c040d9423a1ed6bf4aa4649b59bba1aa1905
Conor Pirie <conor.pirie@gmail.com>
2018-07-04 13:46:04 +1000

    updated signature component to use URL from project attribute

--- client/views/components/generic/input-signature/InputSignature.jsx
+++ client/views/components/generic/input-signature/InputSignature.jsx
@@ -4,6 +4,7 @@ import { func, object, string, bool } from 'prop-types';
 import axios from 'axios';
 import SignaturePad from 'react-signature-pad';
 import { Label, SubmitStatusIcon, IconSmall } from 'views/components';
+import { BaseEntityQuery, GennyBridge } from 'utils/genny';
 
 class InputSignature extends Component {
     static defaultProps = {
@@ -92,23 +93,28 @@ class InputSignature extends Component {
 
     postRequest = ( data ) => {
 
-        const url = 'https://signatures.pleased.property/signature';
+        //const url = 'https://signatures.pleased.property/signature';
+        const url = BaseEntityQuery.getBaseEntityAttribute(GennyBridge.getProject(), 'PRI_SIGNATURE_URL');
+        console.log(url);
 
-        axios({
-            method: 'post',
-            url: url,
-            data: data,
-        }).then( response => {
-            const uploadURL = response.data.signatureURL;
-            this.setState({
-                uploadedUrl: uploadURL,
-            }, () => {
-                if (this.props.validation) this.props.validation( uploadURL, this.props.identifier, this.props.validationList);
-                if (this.props.onChange) {
-                    this.props.onChange( uploadURL );
-                }
+        if (url != null && url.value != null) {
+            axios({
+                method: 'post',
+                url: url.value,
+                data: data,
+            }).then( response => {
+                console.log(response);
+                const uploadURL = response.data.signatureURL;
+                this.setState({
+                    uploadedUrl: uploadURL,
+                }, () => {
+                    if (this.props.validation) this.props.validation( uploadURL, this.props.identifier, this.props.validationList);
+                    if (this.props.onChange) {
+                        this.props.onChange( uploadURL );
+                    }
+                });
             });
-        });
+        }
     }
 
     handleClear = () => {
