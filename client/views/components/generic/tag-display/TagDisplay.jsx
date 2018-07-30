802d89e7c2c5177a3a929f908a2eedb93b3ca792
conorpirie <conorpirie@users.noreply.github.com>
2018-07-30 14:37:56 +1000

    Update TagDisplay.jsx

--- client/views/components/generic/tag-display/TagDisplay.jsx
+++ client/views/components/generic/tag-display/TagDisplay.jsx
@@ -5,96 +5,66 @@ import { IconSmall } from 'views/components';
 
 class TagDisplay extends Component {
 
-    static defaultProps = {
-        className: '',
-        data: [
-            {
-                text: 'Cloud' 
-            },
-            {
-                text: 'Music'
-            },
-            {
-                text: 'Video' 
-            },
-            {
-                text: 'Casdasdasdasd' 
-            },
-            {
-                text: 'Tssdfsdfd'
-            },
-            {
-                text: 'Gasd Jdasdasd' 
-            },
-            {
-                text: 'Lasdasd' 
-            },
-            {
-                text: 'Pdf'
-            },
-            {
-                text: 'Tofkfkf Hfsdfsdfd' 
-            },
-            {
-                text: 'Rewere' 
-            },
-            {
-                text: 'Bsdfsdfdfdf'
-            },
-            {
-                text: 'Bhhgg Gfgfgfg Bfgfg' 
-            }
-        ]
-    }
+  static defaultProps = {
+    className: '',
+    data: [
+    ]
+  }
 
-    static propTypes = {
-        className: string,
-        style: object,
-        data: array,
-    }
+  static propTypes = {
+    className: string,
+    style: object,
+    data: array,
+  }
 
-    state = {
-    }
+  state = {
+  }
 
-    renderTags = (data) => {
-        let asd = asd;
-
-        if (Array.isArray(data)) {
-            return data.map((item, index) => {
-                return (
-                    <div key={index} className='tag-display-tag' >
-                        { item.icon ? 
-                            <IconSmall
-                                name={item.icon}
-                                fontSize='12'
-                            />
-                        : null }
-                        <span>{item && item.text}</span>
-                    </div>
-                );
-            });
-        }
-        else if (typeof data == 'string') {
-            return (
-                <div className='tag-display-tag' >
-                    <span>{data}</span>
-                </div>
-            );
-        }
-        else return null;
-    }
-
-    render() {
-        const { className, data, style } = this.props;
-        const {  } = this.state;
-        const componentStyle = { ...style, };
-
+  renderTags = (data) => {
+    if (Array.isArray(data)) {
+      return data.map((item, index) => {
         return (
-            <div className={`tag-display-container ${className}`} style={componentStyle}>
-                {this.renderTags(data)}
-            </div>
+          <div key={index} className='tag-display-tag' >
+            {item.icon ?
+              <IconSmall
+                name={item.icon}
+                fontSize='12'
+              />
+              : null}
+            <span>{item && item.text}</span>
+          </div>
         );
+      });
     }
+    else if (
+      typeof data == 'string' &&
+      data.startsWith('[')
+    ) {
+      const dataArray = JSON.parse(data);
+      if (Array.isArray(dataArray)) {
+        return dataArray.map((item, index) => {
+          return (
+            <div key={index} className='tag-display-tag' >
+              <span>{item}</span>
+            </div>
+          );
+        });
+      }
+    }
+    else return null;
+  }
+
+  render() {
+    const { className, data, style } = this.props;
+    const { } = this.state;
+    const componentStyle = { ...style, };
+
+    return (
+      <div className={`tag-display-container ${className}`} style={componentStyle}>
+        {this.renderTags(data)}
+      </div>
+    );
+  }
 }
 
 export default TagDisplay;
