744a8e941f1c20ef1d24d1bd67fb3404abefaae4
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2018-10-03 13:16:14 +1000

    add deselect event

--- client/views/components/genny/genny-bucket-view/GennyBucketView.jsx
+++ client/views/components/genny/genny-bucket-view/GennyBucketView.jsx
@@ -24,6 +24,7 @@ class GennyBucketView extends PureComponent {
     };
 
     state = {
+        selectedItemState: null,
     }
 
     constructor(props) {
@@ -145,13 +146,16 @@ class GennyBucketView extends PureComponent {
             }
 
             let sublayout = this.props.sublayout[layout_code];
+
+            const isSelected = this.state.selectedItemState === be.code;
+
             children.push(
                 {
                 content: {
                     title: be.name,
                     description: be.code,
                     code: be.code,
-                    isSelected: this.state.selectedItemState == be.code,
+                    isSelected: isSelected,
                     selectedColor: hideSelectedStyle ? null : selectedColor,
                     screenSize: this.props.screenSize,
                     onClick: this.onClick,
